2939cc10e377193860f76a19a688b5a1843bdbd9
Gamzat27 <gamzat27@icloud.com>
2025-01-29 15:46:51 +0300

    Add files via upload

--- work_1.py
+++ work_1.py
@@ -1,15 +1,8 @@
+def apply_all_func(int_list, *functions):
+    results = {}
+    for func in functions:
+        results[func.__name__] = func(int_list) # Используем атрибут, чтобы взять имя функции!
+    return results
 
-def add_everything_up(a, b):
-    try:
-        # Пытаемся сложить a и b
-        return a + b
-    except TypeError:
-        # Если возникла ошибка при сложении (разные типы), объединяем их в строку
-        return str(a) + str(b)
-
-
-print(add_everything_up(123.456, 'строка'))
-print(add_everything_up('яблоко', 4215))
-print(add_everything_up(123.456, 7))
-print(add_everything_up(22, 55))
-print(add_everything_up('string', 'string'))
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
